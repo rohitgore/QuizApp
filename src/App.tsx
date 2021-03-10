f94734f2e25c0bcfb79448d38a52d63e9f1ec53c
rohitgore <rrohit.gore@gmail.com>
2021-03-10 13:17:30 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import Home from './Components/Home';
+import '@mdi/font/scss/materialdesignicons.scss';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Play from './Components/Quiz/Play';
+import Quizinstruction from './Components/Quiz/Quizinstruction';
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+       <Route path="/" exact component={Home}/>
+       <Route path="/play/instruction" component={Quizinstruction}/>
+       <Route path="/play/Quiz" component={Play} />
+     </Router>
   );
 }
-
 export default App;
